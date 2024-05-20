221c44a55843bdf1c08a65bcedc51b8d7323e237
ayme-h <headayme912@gmail.com>
2024-05-20 16:33:32 -0400

    first thing

--- SketchAScene/ContentView.swift
+++ SketchAScene/ContentView.swift
@@ -9,15 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            
+            Color.appink
+            
+            VStack{
+            
+                Text("Sketch-A-Scene")
+                    .foregroundStyle(.appbrown)
+                    .bold()
+                    .font(.largeTitle)
+                    
+                
+                    
+                    
+                
+            }
+                
+                
+                
+                
+                
+            }
+            //        VStack {
+            //            Image(systemName: "globe")
+            //                .imageScale(.large)
+            //                .foregroundStyle(.tint)
+            //            Text("Hello, world!")
+            //        }
+            //        .padding()
         }
-        .padding()
     }
-}
+    
+   
 
 #Preview {
     ContentView()
