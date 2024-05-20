0c48aae4bfc3f91b81e0c3d854e0c99748fea34d
ayme-h <headayme912@gmail.com>
2024-05-20 15:10:06 -0400

    Initial commit

---
+++ SketchAScene/SketchASceneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchASceneApp.swift
+//  SketchAScene
+//
+//  Created by Ayme Head on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SketchASceneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
