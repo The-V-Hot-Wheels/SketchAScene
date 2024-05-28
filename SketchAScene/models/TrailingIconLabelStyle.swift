72818e18829555537bd1282f330dcdc8521980ba
Jamar <jamarmorgan37@gmail.com>
2024-05-28 16:52:48 -0400

    Test Commit

--- SketchAScene/models/TrailingIconLabelStyle.swift
+++ SketchAScene/models/TrailingIconLabelStyle.swift
@@ -5,4 +5,19 @@
 //  Created by Jamar Morgan on 5/28/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+                        configuration.icon
+                
+                }
+        }
+        
+    }
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
