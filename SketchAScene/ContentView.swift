650f27ffa4b352d88e07e49328e861fd7e112c0c
Jamar <jamarmorgan37@gmail.com>
2024-05-30 13:24:14 -0400

    timer added and other additions

--- SketchAScene/ContentView.swift
+++ SketchAScene/ContentView.swift
@@ -8,18 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         
         ZStack {
             Color.appink
                 .ignoresSafeArea()
             VStack {
+                
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                
                 Text("Sketch-A-Scene ")
                     .font(Font.custom("kindergarten", size: 60))
                     .font(.largeTitle)
                     .padding(20)
                     .foregroundColor(.appbrown)
-//                Spacer()
+                //                Spacer()
                 
                 Text("Pick A Genre")
                     .font(Font.custom("kindergarten", size: 32))
@@ -27,47 +40,52 @@ struct ContentView: View {
                     .padding(100)
                     .foregroundColor(.appbrown)
                     .padding([.top, .bottom])
-                    
-                                    
-                    
-                    
-                        
-                   
-               
-               Spacer()
+                
+                
+                
+                
+                
+                
+                
+                Spacer()
                 
                 GenreRowView()
                 
-            
-        }
-            
-            
-//                Text("Sketch-A-Scene")
-                
-               
-//                    .foregroundStyle(.appbrown)
-//                    .bold()
-//                    .font(.largeTitle)
-//                    
-                
-                    
-                    
                 
             }
-                
-                
-                
-                
-                
-            }
-            //        VStack {
-            //            Image(systemName: "globe")
-            //                .imageScale(.large)
-            //                .foregroundStyle(.tint)
-            //            Text("Hello, world!")
-            //        }
-            //        .padding()
+            
+            
+            //                Text("Sketch-A-Scene")
+            
+            
+            //                    .foregroundStyle(.appbrown)
+            //                    .bold()
+            //                    .font(.largeTitle)
+            //
+            
+            
+            
+            
         }
+        .onReceive(timer) { time in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        
+        
+        
+        
+        
+    }
+    //        VStack {
+    //            Image(systemName: "globe")
+    //                .imageScale(.large)
+    //                .foregroundStyle(.tint)
+    //            Text("Hello, world!")
+    //        }
+    //        .padding()
+}
     
     
    
---
+++ SketchAScene/GameTimer.swift
@@ -0,0 +1,20 @@
+//
+//  GameTimer.swift
+//  SketchAScene
+//
+//  Created by Jamar Morgan on 5/29/24.
+//
+
+import SwiftUI
+
+
+
+struct GameTimer: View {
+    var body: some View {
+       Text("hello world")
+    }
+}
+
+#Preview {
+    GameTimer()
+}
