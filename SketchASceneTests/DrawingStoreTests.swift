56606005df7ee2eb6c95c303c341ede71836804b
Alonso del Arte <alonso.delarte@gmail.com>
2024-06-20 17:04:51 -0400

    Failing addDrawing( ) test

--- SketchASceneTests/DrawingStoreTests.swift
+++ SketchASceneTests/DrawingStoreTests.swift
@@ -9,27 +9,16 @@ import XCTest
 
 final class DrawingStoreTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAddDrawing() throws {
+        let image1 = UIImage(named: "comedy")
+        let image2 = UIImage(named: "drama")
+        let image3 = UIImage(named: "horror")
+        let expected = [image1, image2, image3].shuffled()
+        for image in expected {
+            DrawingStore.addDrawing(image!)
         }
+        let actual = DrawingStore.drawings
+        XCTAssertEqual(expected, actual)
     }
 
 }
