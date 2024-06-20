f16d766dddd0387209ff4b1c3e193bd7ed0b58f5
Alonso del Arte <alonso.delarte@gmail.com>
2024-06-20 16:26:39 -0400

    New class, new test class

---
+++ SketchAScene/DrawingStore.swift
@@ -0,0 +1,29 @@
+//
+//  DrawingStore.swift
+//  SketchAScene
+//
+//  Created by Alonso del Arte on 6/20/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct DrawingStore {
+    
+    static var drawings: [UIImage] = []
+    
+    static func addDrawing(_ drawing: UIImage) {
+        // TODO: Write tests for this
+    }
+    
+    // TODO: Write tests for this
+    static func retrieveDrawings() -> [UIImage] {
+        let array: [UIImage] = []
+        return array
+    }
+    
+    static func clear() {
+        // TODO: Write tests for this
+    }
+    
+}
